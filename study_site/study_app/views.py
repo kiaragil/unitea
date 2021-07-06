588e24b4d830f4c96cbeb0b632d2787b2dbba87d
Kiara Gil <kiarargil@gmail.com>
2021-07-06 09:03:18 -0700

    Update views.py

--- study_site/study_app/views.py
+++ study_site/study_app/views.py
@@ -1,25 +1,50 @@
 from django.shortcuts import render
+
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def searchresults(request):
+    return render(request, 'searchresults.html')
+
+
+def contactus(request):
+    return render(request, 'contactus.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
 
 def kiara(request):
-    return render(request,'T4TM-Kiara.html')
-    
+    return render(request, 'T4TM-Kiara.html')
+
+
 def ostyn(request):
-    return render(request,'T4TM-Ostyn.html')
+    return render(request, 'T4TM-Ostyn.html')
+
 
 def josh(request):
-    return render(request,'T4TM-Josh.html')
+    return render(request, 'T4TM-Josh.html')
+
 
 def miho(request):
-    return render(request,'T4TM-Miho.html')
+    return render(request, 'T4TM-Miho.html')
+
 
 def vernon(request):
-    return render(request,'T4TM-Vernon.html')
+    return render(request, 'T4TM-Vernon.html')
+
 
 def cong(request):
-    return render(request,'T4TM-Cong.html')
+    return render(request, 'T4TM-Cong.html')
+
 
 def melinda(request):
-    return render(request,'T4TM-Melinda.html')
+    return render(request, 'T4TM-Melinda.html')
