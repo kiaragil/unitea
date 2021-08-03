bb883c8dc2592a28f359a918e135f68ac5a3e42a
ostynsy <ostynsy@gmail.com>
2021-08-02 19:19:43 -0700

    added github url to headers

--- study_site/study_app/validators.py
+++ study_site/study_app/validators.py
@@ -3,6 +3,7 @@ Class: CSC648-848 SW Engineering SU21
 Team: Team 4
 Name: Kiara Gil, Ostyn Sy, Joshua Stone, Cong Le, Miho Shimizu, Vernon Xie, Melinda Yee
 GitHub Name: KiaraGil, OstynSy, JoshLikesToCode, CleGuren, simicity, vxie123, melinda15
+GitHub URL: https://github.com/sfsu-joseo/csc648-848-sw-engineering-SU21-T04
 
 File Name: validators.py
 
